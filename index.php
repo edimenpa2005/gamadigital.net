16e51678c7e8ed44fb3d5b58be2c8395050ec1bf
edimenpa2005 <edimenpa2005@gmail.com>
2013-11-07 12:11:02 -0500

    Cambios 01
    Etapa 01 creación index.php

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<!-- Juego de Caracteres -->
+<meta charset="utf-8">
+<!-- Titulo -->
+	<title>Gama Digital, Creacion Paginas Web, Registro de Dominios, Hosting Profesional, Blog</title>
+    <meta name="description" content="Gama Digital - Creación de Paginas Web Empresariales  - Blog - Registro de Dominios - Hosting - Ideas de Negocio - SEO - Marketing  " />
+    <meta name="keywords" content="Paginas web, Blog, Registro de Dominios, Hosting, Ideas de Negocio, Trafico" />
+    <meta name="author" content="Gama Digital">
+<!-- Favicon -->
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+</head>
+
+<body>
+<header>
+	<!-- Encabezado tiene que tiene que tener 3 divs-->
+	<?php 
+	// Los identificadores y las clases se parecen, usamos identificadores para dar propiedades a un solo elemento, y clases para un dar propiedades a un grupo de elementos
+
+	 ?>
+<!--Nesecitamos 3 divs para el header	-->
+<div id="logo">
+	<!-- <a href="#"><img src="css/img/logo.png" width="75" height="75" alt="Colegio San Gabriel" id="ilogo"></a> -->
+</div>
+<div id="titulo">
+<h1>Gama Digital</h1>
+<span>Hoy es el momento</span>
+</div>
+<div id="identificacion"><p>Usted a ingresado como</p></div>
+
+
+</header>
+<nav>
+	<ul>
+		<li><a href="#">Bienvenidos</a></li>
+		<li><a href="#">Registro de Dominios</a></li>
+		<li><a href="#">Hosting Profesional</a></li>
+		<li><a href="#">Diseño y Programación Web</a></li>
+		<li><a href="#">Portafolio</a></li>
+
+	</ul>
+</nav>
+<div id="slide">
+	Amimación : Lo importante de estar en internet
+	</br>
+	Internet es el mayor mercado del planeta que seguramente estan buscando una empresa como la tuya
+	</br>
+	Tener un sitio web es como inaugurar una tienda de escala mundial gente de todo el mundo puede visitarte
+	</br>
+	Para una empresa no basta con tener un blog o una cuenta en una red social si quieres vender en serioi necesitas un Sitio web empresarial, 
+	</br>
+	Imagite el sitio web empresarial integra, blog, foros, redes sociales, video, correo empresarial, todo trabajando para ti
+	</br> 
+	Usamos	Marketing online para promocionar tus productos o servicios
+	</br>
+	Existen varias formas de incluir su empresa en internet Sabe como hacerlo Nosotros si Que estes aqui es prueba de ello Podemos utilizar las mismas técnicas y estrategias para que su empresa o Ud. este en Internet y mejore sus ventas
+
+
+	
+</div>
+<section id="contenedor">
+	<section id="principal">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+		<article>
+			&lt;article&gt;
+		</article>
+
+	</section>
+	<aside>&lt;article&gt; </aside>
+	
+</section>
+<footer>
+	&lt;footer&gt;
+</footer>
+
+
+
+</body>
+</html>
